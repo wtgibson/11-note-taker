62486be3fcfc73b0124881682ad465b6f36ff4be
Gibbyswag <will.gibson7@gmail.com>
2020-04-16 23:13:52 -0700

    updated server.js to include * index.html

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+
 // Notes page
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
